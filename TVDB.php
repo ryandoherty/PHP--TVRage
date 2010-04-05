0675f9dc351bcfb73885b08700a3415ec0cfd42e
Ryan Doherty <rdoherty@ryandoherty-macbookpro.local>
2010-04-04 22:00:06 -0700

    first commit

---
+++ TVDB.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Library that encapsulates tvrage.com API for easy access to TV show information
+	 *
+	 * @author Ryan Doherty <ryan@ryandoherty.com>
+	 * @version 1.0
+	 * @copyright Ryan Doherty, 16 February, 2008
+	 * @package PHP::TVRAGE
+	 **/
+
+	//Include our files and we're done!
+	require 'TVRAGE/TVRAGE.class.php';
+	require 'TVRAGE/TV_Show.class.php';
+	require 'TVRAGE/TV_Shows.class.php';
+	require 'TVRAGE/TV_Episode.class.php';
+?>
